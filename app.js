699c0d59ad68f973030f5601d7a28054208b7b2a
Shristi-Roy <122510934+Shristi-Roy@users.noreply.github.com>
2023-09-15 08:28:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,24 @@
 import React from 'react'; // For importing React itself
 import ReactDOM from 'react-dom/client'; // For importing ReactDOM for rendering in a web browser
 
+//JSX (transpiled before it reaches the JS) - PARCEL - BABLE
 
-const heading = React.createElement(
-    "h1",
-     {id:"heading"},
-     "Namaste React 🚀",
-);
+// JSX => bable transpiles it to React.createElement => (is executed) ReactElement-JS object => HTML Element(rendered)
+
+const jsxheading = <h1 id="head" className="heading" tabIndex="5">Namaste React using JSX 🚀 </h1>
+
+console.log(jsxheading);
+
+//This is one line code if we want to write JSX is in multiple lines then we wrap the code it inside () this paranthesis. Like:
+
+const jsxheadings = ( <h1 className="heading">
+    Namaste React using JSX 🚀 
+    </h1>);
+
+console.log(jsxheadings);
+
+// Why we use () round brackets?
+//becoz bable needs to understand where is JSX starting & where is ending.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxheadings);
